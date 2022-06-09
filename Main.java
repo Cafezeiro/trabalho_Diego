c9c6fe7477428fd196f36cb380eca983d66988eb
Cafezeiro <100365410+Cafezeiro@users.noreply.github.com>
2022-06-08 22:53:12 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,231 +1,90 @@
 
-
-import java.util.Iterator;
+import java.util.Random;
 import java.util.Scanner;
 
-class Main {
-
-  static Scanner console = new Scanner(System.in);
-
-  static final int TOTAL_AVALIACOES = 3;
-  static final String[] NOMES_AVALIACOES = { "A1", "A2", "A3" };
-  static final double[] NOTA_MAX_AVALIACOES = { 30.00, 30.00, 40.00 };
-  
-  static double[] notas = new double [TOTAL_AVALIACOES];
-
-  
-  /**
-	 * Ler uma nota do usuário
-	 * @param mensagem O texto que aparecerá na tela
-	 * @return um número double representando a nota.
-	 */
-	static double lerNota(String mensagem, double notaMaxima) {
-
-      double nota = 0.0;
-
-      do {
-
-        System.out.printf("%s = ", mensagem);
-        nota = console.nextDouble();
-        
-      } while (nota < 0.00 || nota > notaMaxima);
-
-    return nota;
-	}
-
-  
-  /**
-  * Atualiza o valor da respectiva nota do estudante
-  * @param indiceNota um número inteiro representando o índice (posição) da nota no vetor
-  */
-  static void atualizarNota(int indiceNota) {
-
-    System.out.println();
-    notas[indiceNota] = lerNota(NOMES_AVALIACOES[indiceNota], NOTA_MAX_AVALIACOES[indiceNota]);
-  
-  } // Fim do método atualizarNota
-
-  
- /**
-  * @param notaFinal A soma de todas as avalições feita pelo estudante ao longo do semestre
-  * @return uma string representando o status final do estudante, são eles: APROVADO, REPROVADO, EM RECUPERAÇÃO.
-  */
-  static String avaliarSituacao(double notaFinal) {
-
-    if(notaFinal < 30)
-      return "REPROVADO";
-    else if (notaFinal < 70)
-      return "EM RECUPERAÇÃO";
-    else
-      return "APROVADO";
-    
-  } // Fim do método avaliarSituacao()
-  
-  static double calcularMedia(double[] notas) {
-	  
-	  double media = 0;
-	  
-	  for (int i = 0; i < TOTAL_AVALIACOES; i++) {
-		  
-		  media += notas[i];
-	  }
-	  
-	  media /= TOTAL_AVALIACOES;
-	  
-	  return media;
-  }
-  
- static String maiorNota(double[] notas) {
-	String maiorNota = "";
-	double verificarMaior = 0;
+public class Main {
 	
-	for (int i = 0; i < TOTAL_AVALIACOES; i++) {
-		
-	if (notas[i] > verificarMaior) {
-		verificarMaior = notas[i];
-		maiorNota = NOMES_AVALIACOES[i];
+	static Scanner console = new Scanner(System.in);
+	static Random sorteador = new Random();
+	
+	static final byte[][] REGRAS_DO_JOGO = {
+			{ 0, -1,  1, 1, -1},
+			{ 1,  0, -1, -1, 1 },
+			{-1,  1,  0, 1, -1 },	
+			{-1,  1, -1, 0,  1 },	
+			{ 1, -1,  1, -1, 0 },	
+	};
+	
+	static final String[] opcoes = { "Pedra", "Papel", "Tesoura", "Lagarto", "Spock" };
+	
+	static final byte GANHOU = 1;
+	static final byte EMPATOU = 0;
+	static final byte PERDEU = -1;
+	
+	
+	
+	static int obterOpcaoComputador() {
+		return sorteador.nextInt(REGRAS_DO_JOGO.length);
 	}
+	
+	static int lerOpcaoJogador() {		
+		System.out.println("[0] Pedra");
+		System.out.println("[1] Papel");
+		System.out.println("[2] Tesoura");
+		System.out.println("[3] Lagarto");
+		System.out.println("[4] Spock");
+		System.out.print("Opção: ");
 		
+		return console.nextInt();		
 	}
-	 
-	return maiorNota;
- }
-  
-  
-  
-  /**
-  * Mostra na tela um relatório das notas do estudante
-  */
-  static void mostrarNotas() {
+	
+	static void jogar() {
 
-    double notaFinal = 0.0;
+		int jogador = lerOpcaoJogador();
+		int computador = obterOpcaoComputador();
+		
+		System.out.printf("\nJogador 1: %s", opcoes[jogador]);
+		System.out.printf("\nComputador: %s", opcoes[computador]);
+		
+		System.out.println("\n");
+		
+	
+		
+	
+		if (REGRAS_DO_JOGO[jogador][computador] == GANHOU) {
+			System.out.println("Parabéns você ganhou");
+		} else if (REGRAS_DO_JOGO[jogador][computador] == EMPATOU) {
+			System.out.println("Empate");
+		} else if (REGRAS_DO_JOGO[jogador][computador] == PERDEU) {
+			System.out.println("O computador ganhou!");
+		}
+	}
+	
+	static void menu() {
+		
+		System.out.println("Menu");
+		System.out.println("[1] Jogar");
+		System.out.println("[2] SAIR");
+		
+		System.out.print("Opção: ");
+		int opcao = console.nextInt();
+		
+		switch(opcao) {
+		case 1:
+			jogar();
+			menu();
+			break;
+		case 2:
+			System.exit(0);
+			break;
+			default:
+				menu();
+			break;
+		}
+	}
+	
 
-    System.out.println("\n\t\tNOTAS");
-    System.out.println();
-
-    for (int i = 0; i < TOTAL_AVALIACOES; i++) {
-
-      System.out.printf("Avaliação %s = %.2f pts", NOMES_AVALIACOES[i], notas[i]);
-      System.out.println();
-      notaFinal += notas[i];
-    
-    }
-
-    System.out.printf("\n  Nota Final = %.2f pts", notaFinal);
-    System.out.printf("\n  Situação = %s", avaliarSituacao(notaFinal));
-    System.out.printf("\n  Média Final = %.2f" , calcularMedia(notas));
-    System.out.printf("\n  Maior Nota = %s", maiorNota(notas));
-  
-  } // Fim do método mostrarNotas()
-  
-  
-
-  static void provaAI(double[] notas) {
-      double notaAi = 0;
-      int piorProva = notas[0] <= notas[1] ? 0 : 1; //Verifica a menor nota entre as 2 primeira notas.
-
-
-      System.out.println("Digite A Nota Da Prova AI");
-      notaAi = lerNota("Prova AI", NOTA_MAX_AVALIACOES[piorProva]);
-
-      if (notaAi > notas[piorProva]) {
-          notas[piorProva] = notaAi;
-          System.out.printf("A Avaliação %s Foi Substituida Pela Avalição AI\n", NOMES_AVALIACOES[piorProva]);
-      } else {
-          System.out.println("A Nota Da Prova AI Não Foi Suficiente Para Alterar Alguma Prova");
-      }
-
-  } // Fim do método provaAI()
-
-  /**
-   * Verifica se deseja fazer a prova AI
-   */  
-  static void opcaoProvaAI(double[] notas) {
-
-      if (calcularMedia(notas) <= 23.0 && calcularMedia(notas) >= 10.0 && (notas[0] + notas[1]) < 60) {
-          System.out.print("""
-                  Deseja Cadastrar Nota Prova AI?
-                  Digite SIM ou NÃO:\s""");
-          String opcao = console.next();
-
-          boolean checkOpcao = false;
-          while (!checkOpcao) {
-              if (opcao.equalsIgnoreCase("sim")) {
-                  provaAI(notas);
-                  mostrarNotas();
-                  checkOpcao = true;
-              } else if (opcao.equalsIgnoreCase("não") || opcao.equalsIgnoreCase("nao")) {
-                  System.out.println("Opção Digitada: Não");
-                  checkOpcao = true;
-              } else {
-                  System.out.println("Opção Invalida");
-                  System.out.print("Digite SIM ou NÃO: ");
-                  opcao = console.next();
-              }
-          }
-
-      }
-
-  } // Fim do método opcaoProvaAI()
-  
- /**
-  * Exibe o menu principal da aplicação
-  */
-  static void mostrarMenu() {
-
-    System.out.println("\n\n");
-    System.out.println("\t\tMENU");
-    System.out.println();
-    
-    System.out.println("[1] Cadastrar Notas A1");
-    System.out.println("[2] Cadastrar Nota A2");
-    System.out.println("[3] Cadastrar Nota A3");
-    System.out.println("[4] Mostrar Notas");
-    System.out.println("[0] SAIR");
-
-    System.out.print("\nDigite uma opção:  ");
-    byte opcao = console.nextByte();
-
-
-    switch(opcao) {
-
-      case 0:
-        System.exit(0);
-        break;
-      
-      case 1:
-        atualizarNota(0);
-        break;
-      
-      case 2:
-        atualizarNota(1);
-        break;
-
-      case 3:
-        atualizarNota(2);
-        break;
-
-      case 4:
-        mostrarNotas();
-        opcaoProvaAI(notas);
-        break;
-
-      default:
-        mostrarMenu();
-        break;
-
-    }
-
-    mostrarMenu();
-
-  } // Fim do método mostrarMenu()
-
-  
-  public static void main(String[] args) {
-    
-    mostrarMenu();
-  
-  } // Fim do método main();
-  
-} // Fim da classe Main
-
+	public static void main(String[] args) {		
+		menu();
+	}
+}
